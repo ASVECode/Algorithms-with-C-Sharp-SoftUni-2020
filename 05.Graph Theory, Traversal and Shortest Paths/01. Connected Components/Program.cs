00f01d33e5f18c5dfc7702ebabf8d2e421040fc9
ASVECode <57017077+ASVECode@users.noreply.github.com>
2020-12-10 18:47:04 +0200

    add graph theory, traversal and shortest paths

---
+++ 05.Graph Theory, Traversal and Shortest Paths/01. Connected Components/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace _01.Connected_Components
+{
+    class Program
+    {
+        private static List<int>[] graph;
+        private static bool[] visited;
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            graph = new List<int>[n];
+            visited = new bool[n];
+
+            for (int node = 0; node < n; node++)
+            {
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    graph[node] = new List<int>();
+                }
+                else
+                {
+                    var children = line
+                        .Split()
+                        .Select(int.Parse)
+                        .ToList();
+
+                    graph[node] = children;
+                }
+            }
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                if (visited[node])
+                {
+                    continue;
+                }
+                var components = new List<int>();
+                DFS(node, components);
+
+                Console.WriteLine($"Connected component: {string.Join(" ", components)}");
+            }
+        }
+
+        private static void DFS(int node, List<int> components)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+            visited[node] = true;
+
+            foreach (var child in graph[node])
+            {
+                DFS(child, components);
+            }
+
+            components.Add(node);
+        }
+    }
+}
---
+++ 05.Graph Theory, Traversal and Shortest Paths/DFS and BFS/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DFS_and_BFS
+{
+    class Program
+    {
+        private static Dictionary<int, List<int>> graph;
+        private static HashSet<int> visited;
+
+        static void Main()
+        {
+            graph = new Dictionary<int, List<int>>
+            {
+                {1, new List<int>{19,21,14} },
+                {19, new List<int>{7,12,31,21}},
+                {7, new List<int>{1}},
+                {12, new List<int>()},
+                {21, new List<int>{14} },
+                {31, new List<int>{21} },
+                {14, new List<int>{6,23} },
+                {6, new List<int>() },
+                {23, new List<int>{21} }
+
+            };
+
+            visited = new HashSet<int>();
+
+            foreach (var node in graph.Keys)
+            {
+                if (!visited.Contains(node))
+                {
+                    BFS(node);
+                }
+                //DFS(node);
+            }
+        }
+
+        private static void DFS(int node)
+        {
+            if (visited.Contains(node))
+            {
+                return;
+            }
+            visited.Add(node);
+            foreach (var child in graph[node])
+            {
+                DFS(child);
+            }
+            Console.WriteLine(node);
+        }
+        private static void BFS(int startNode)
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(startNode);
+            visited.Add(startNode);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                Console.WriteLine(node);
+
+                foreach (var child in graph[node])
+                {
+                    if (!visited.Contains(child))
+                    {
+                        queue.Enqueue(child);
+                        visited.Add(child);
+                    }
+                }
+            }
+
+        }
+
+    }
+}
