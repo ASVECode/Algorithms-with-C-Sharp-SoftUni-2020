db45fe3941af2bc8163992d7ee196047b2082c04
ASVECode <57017077+ASVECode@users.noreply.github.com>
2020-12-12 23:50:54 +0200

    add topological sorting

---
+++ 05.Graph Theory, Traversal and Shortest Paths/02. Topological Sorting/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Topological_Sorting
+{
+    class Program
+    {
+        private static string[] stringSeparator1 = new string[] { " -> " };
+        private static string[] stringSeparator = new string[] { ", " };
+
+        private static Dictionary<string, List<string>> graph;
+
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            graph = ReadGraph(n);
+        }
+
+        private static Dictionary<string, List<string>> ReadGraph(int n)
+        {
+            var dependencies = new Dictionary<string, int>();
+
+            foreach (var kvp in graph)
+            {
+                var node = kvp.Key;
+                var children = kvp.Value;
+
+                foreach (var child in children)
+                {
+                    if (!dependencies.ContainsKey(child))
+                    {
+                        dependencies.Add(child, 1);
+                    }
+                    else
+                    {
+                        dependencies[child]++;
+                    }
+                }
+            }
+        }
+    }
+}
