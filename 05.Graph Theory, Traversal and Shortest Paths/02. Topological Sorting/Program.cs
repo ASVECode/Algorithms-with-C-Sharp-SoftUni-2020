4bdab119dfb88e0a61c09fa9053da9ec1b952914
ASVECode <57017077+ASVECode@users.noreply.github.com>
2020-12-14 23:17:50 +0200

    .

--- 05.Graph Theory, Traversal and Shortest Paths/02. Topological Sorting/Program.cs
+++ 05.Graph Theory, Traversal and Shortest Paths/02. Topological Sorting/Program.cs
@@ -1,45 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02.Topological_Sorting
 {
     class Program
     {
-        private static string[] stringSeparator1 = new string[] { " -> " };
-        private static string[] stringSeparator = new string[] { ", " };
-
-        private static Dictionary<string, List<string>> graph;
-
-        static void Main()
+        public static Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
+        public static Dictionary<string, int> dependencies = new Dictionary<string, int>();
+        public static void Main()
         {
-            var n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
             graph = ReadGraph(n);
+
+            //graph = new Dictionary<string, List<string>>()
+            //{
+            //    { "A", new List<string>() { "B", "C" } },
+            //    { "B", new List<string>() { "D", "E" } },
+            //    { "C", new List<string>() { "F" } },
+            //    { "D", new List<string>() { "C", "F" } },
+            //    { "E", new List<string>() { "D" } },
+            //    { "F", new List<string>() { } }
+            //};
+            dependencies = ExtractDependences();
+            var sorted = TopologicalSorting();
+
+            if (sorted == null)
+            {
+                Console.WriteLine("Invalid topological sorting");
+            }
+            else
+            {
+                Console.WriteLine($"Topological sorting: { string.Join(", ", sorted)}");
+            }
+
         }
 
-        private static Dictionary<string, List<string>> ReadGraph(int n)
+        public static List<string> TopologicalSorting()
         {
-            var dependencies = new Dictionary<string, int>();
+            var sorted = new List<string>();
 
+            while (dependencies.Count > 0)
+            {
+                var nodeToRemove = dependencies.FirstOrDefault(n => n.Value == 0);
+
+                if (string.IsNullOrEmpty(nodeToRemove.Key))
+                {
+                    break;
+                }
+                var node = nodeToRemove.Key;
+                var children = graph[node];
+
+                sorted.Add(node);
+                foreach (var child in children)
+                {
+                    dependencies[child]--;
+                }
+                dependencies.Remove(nodeToRemove.Key);
+            }
+            if (dependencies.Count > 0)
+            {
+                return null;
+            }
+            return sorted;
+        }
+
+        public static Dictionary<string, int> ExtractDependences()
+        {
+            var result = new Dictionary<string, int>();
             foreach (var kvp in graph)
             {
                 var node = kvp.Key;
                 var children = kvp.Value;
 
+                if (!result.ContainsKey(node))
+                {
+                    result.Add(node, 0);
+                }
+
                 foreach (var child in children)
                 {
-                    if (!dependencies.ContainsKey(child))
+                    if (!result.ContainsKey(child))
                     {
-                        dependencies.Add(child, 1);
+                        result.Add(child, 1);
                     }
                     else
                     {
-                        dependencies[child]++;
+                        result[child]++;
                     }
                 }
             }
+
+            return result;
+        }
+
+        public static Dictionary<string, List<string>> ReadGraph(int n)
+        {
+            var graphResult = new Dictionary<string, List<string>>();
+            string[] separatingString = { "->" };
+            string[] separatingString1 = { ", " };
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] parts = Console.ReadLine()
+                    .Split(separatingString, StringSplitOptions.RemoveEmptyEntries);
+                var key = parts[0].Trim();
+                if (parts.Length == 1)
+                {
+                    graphResult[key] = new List<string>();
+                }
+                else
+                {
+                    var children = parts[1]
+                        .Trim()
+                        .Split(separatingString1, StringSplitOptions.None)
+                        .ToList();
+                    graphResult[key] = children;
+                }
+
+            }
+            return graphResult;
         }
     }
 }
---
+++ 05.Graph Theory, Traversal and Shortest Paths/03. Shortest Path/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Shortest_Path
+{
+    class Program
+    {
+        private static Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+        private static HashSet<int> visited = new HashSet<int>();
+
+
+        static void Main()
+        {
+            int numNodes = int.Parse(Console.ReadLine());
+            int numEdges = int.Parse(Console.ReadLine());
+
+            graph = ReadGraph(numNodes, numEdges);
+            int startNode = int.Parse(Console.ReadLine());
+            int endNode = int.Parse(Console.ReadLine());
+            visited = new HashSet<int>(numNodes);
+
+            //  foreach (var children in graph.Keys)
+            //  {
+            //      Console.WriteLine(String.Join(" ", children));
+            //  }
+
+            Console.WriteLine(CalculateShortestDistance(startNode, endNode));
+        }
+
+        private static Dictionary<int, List<int>> ReadGraph(int numNodes, int numEdges)
+        {
+            Dictionary<int, List<int>> graphResult = new Dictionary<int, List<int>>();
+
+            for (int i = 1; i <= numEdges; i++)
+            {
+                var children = new List<int>();
+                var edge = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var node = edge[0];
+                graphResult[node] = children;
+                var child = edge[1];
+
+                if (!graphResult[node].Contains(child))
+                {
+                    graphResult[node].Add(child);
+                }
+                if (!visited.Contains(child))
+                {
+                    node = child;
+                    graphResult[node] = new List<int>();
+                }
+            }
+            return graphResult;
+        }
+        private static int CalculateShortestDistance(int startNode, int endNode)
+        {
+            visited = new HashSet<int>();
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(startNode);
+            List<int> children;
+            int distance = 1;
+
+            while (queue.Count > 0)
+            {
+                children = new List<int>();
+
+                while (queue.Count > 0)
+                {
+                    ;
+                    var current = queue.Dequeue();
+
+                    foreach (var child in graph[current])
+                    {
+                        if (!visited.Contains(child))
+                        {
+                            if (child == endNode)
+                            {
+                                return distance;
+                            }
+                            visited.Add(child);
+                            children.Add(child);
+                        }
+                    }
+                }
+
+                queue = new Queue<int>(children);
+                distance++;
+            }
+
+            return -1;
+        }
+
+        private static void BFS(int startNode, int endNode)
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(startNode);
+            visited.Add(startNode);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                //Console.WriteLine(node);
+
+                foreach (var child in graph[node])
+                {
+                    if (!visited.Contains(child))
+                    {
+                        queue.Enqueue(child);
+                        visited.Add(child);
+                    }
+                }
+            }
+
+        }
+
+    }
+}
