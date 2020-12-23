f65e80603418c35d9df0be99a286937c3ea035d3
ASVECode <57017077+ASVECode@users.noreply.github.com>
2020-12-23 20:55:02 +0200

    .

---
+++ Exam Preparation/Exam Preparation/01. Two minutes to Midnight/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Two_minutes_to_Midnight
+{
+    class Program
+    {
+        private static Dictionary<string, long> cache = new Dictionary<string, long>();
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            var ways = GetBinom(n, k);
+        }
+
+        private static long GetBinom(int n, int k)
+        {
+            var id = $"{n} {k}";
+            if (cache.ContainsKey(id))
+            {
+                return cache[id];
+            }
+            if(n==0 || k == 0)
+            {
+                return 1;
+            }
+
+        }
+    }
+}
