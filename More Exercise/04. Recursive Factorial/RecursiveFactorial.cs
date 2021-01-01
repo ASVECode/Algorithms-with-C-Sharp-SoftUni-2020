4294439f2f1b11d6f75375af87ed3f8adde40028
ASVECode <57017077+ASVECode@users.noreply.github.com>
2021-01-02 00:00:00 +0200

    .

---
+++ More Exercise/04. Recursive Factorial/RecursiveFactorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04.Recursive_Factorial
+{
+    class RecursiveFactorial
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fact(n));
+        }
+
+        private static int Fact(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * Fact(n - 1);
+        }
+    }
+}
